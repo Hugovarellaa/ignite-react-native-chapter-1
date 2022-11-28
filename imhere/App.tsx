e671815c92b74cd0ca69cd2e9e626873a7ab7c6c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-28 15:47:14 -0300

    Separando arquivos

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,30 +1,5 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Home } from './src/screens/Home'
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.eventName}>Nome do Evento</Text>
-            <Text style={styles.eventDate}>Segunda, 28 de Novembro de 2022</Text>
-            <StatusBar style="light" />
-        </View>
-    )
+    return <Home />
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#131016',
-        padding: 24,
-    },
-    eventName: {
-        color: '#fdfcfe',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48,
-    },
-    eventDate: {
-        color: '#6b6b6b',
-        fontSize: 16,
-    },
-})
